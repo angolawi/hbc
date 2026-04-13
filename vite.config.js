723712bf60aa56937d6bcf51d6f18ede6f659bcd
Willian S. <wsapcf@gmail.com>
2026-04-13 15:04:25 -0300

    back to main

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hbc/',
+  base: './',
   plugins: [
     react(),
     tailwindcss()
